9bdd083e65b1a7ecdae56530fc8fee8c27fca847
Johann Becker <johann.drums@gmail.com>
2025-08-20 09:12:25 +0000

    update 20-08

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Recriando o projeto TabNews do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-function Home(){
-    return <h1>alteracao da home</h1>
+function Home() {
+  return <h1>alteracao da home</h1>;
 }
 export default Home;
