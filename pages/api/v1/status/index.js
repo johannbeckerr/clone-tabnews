e09cc6efb9b281bc97adef747bba5528682ad107
Johann Becker <johann.drums@gmail.com>
2026-01-19 19:07:37 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as SUM;");
+  console.log(result.rows);
   response.status(200).json({ chave: "ESSE É MEU PRIMEIRO TESTE DE API" });
 }
 export default status;
